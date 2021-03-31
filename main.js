5eed95208ae8ad10249223c6ff0725cab2f335a4
karolyim <matej.karolyi@gmail.com>
2021-03-31 13:38:33 +0200

    [FEATURE] button manipulating with the unicorn

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 // tady je místo pro náš program
+
+function venaDoOhrady() {
+    let jednorozec = document.querySelector('#jednorozec');
+
+    jednorozec.style.left = '420px';
+    jednorozec.style.top = '260px';
+}
